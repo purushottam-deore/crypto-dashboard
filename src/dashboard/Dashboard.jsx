2175a63f51b6ede3405b710eeb27df7575c9c7fd
purushottam deore <rajput.purushottamp@gamil.com>
2025-08-21 10:54:43 +0530

    Fix import path: correct case for Chart component

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import Chart from "../component/chart";
+import Chart from "../component/Chart";
 import ExchangeCoins from "../component/ExchangeCoins";
-import Sidebar from "../component/sidebar";
+import Sidebar from "../component/Sidebar";
 import Portfolio from "../component/Portfolio";
 import Searchbar from "../component/Searchbar";
 import CurrencySelector from "../component/CurrencySelector";
